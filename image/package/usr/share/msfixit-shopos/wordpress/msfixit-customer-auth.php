557f22efd384bd6e5404822b89325ed4368ca77a
Ms-FixIT-25 <office@msfixit.at>
2026-08-04 09:00:31 +0200

    Load account deletion module

--- image/package/usr/share/msfixit-shopos/wordpress/msfixit-customer-auth.php
+++ image/package/usr/share/msfixit-shopos/wordpress/msfixit-customer-auth.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Ms. FixIT ShopOS Customer Authentication
  * Description: Google sign-in, customer TOTP, recovery codes and account security controls for WooCommerce.
- * Version: 1.0.0
+ * Version: 1.1.0
  */
 
 declare(strict_types=1);
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-const MSFIXIT_CUSTOMER_AUTH_VERSION = '1.0.0';
+const MSFIXIT_CUSTOMER_AUTH_VERSION = '1.1.0';
 const MSFIXIT_CUSTOMER_AUTH_ENDPOINT = 'sicherheit';
 const MSFIXIT_CUSTOMER_GOOGLE_SCOPE = 'openid email profile';
 const MSFIXIT_CUSTOMER_GOOGLE_AUTH_ENDPOINT = 'https://accounts.google.com/o/oauth2/v2/auth';
@@ -19,6 +19,6 @@ const MSFIXIT_CUSTOMER_GOOGLE_TOKEN_ENDPOINT = 'https://oauth2.googleapis.com/to
 const MSFIXIT_CUSTOMER_GOOGLE_USERINFO_ENDPOINT = 'https://openidconnect.googleapis.com/v1/userinfo';
 
 $msfixitCustomerAuthDirectory = '/usr/share/msfixit-shopos/wordpress/msfixit-customer-auth';
-foreach (['google.php', 'flow.php', 'totp.php', 'account.php', 'admin.php'] as $msfixitCustomerAuthModule) {
+foreach (['google.php', 'flow.php', 'totp.php', 'deletion.php', 'account.php', 'admin.php'] as $msfixitCustomerAuthModule) {
     require_once $msfixitCustomerAuthDirectory . '/' . $msfixitCustomerAuthModule;
 }
