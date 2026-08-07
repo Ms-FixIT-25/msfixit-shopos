6a349c45e38f7a10dc55a699a0ced13256e2ebd1
Ms-FixIT-25 <office@msfixit.at>
2026-08-07 06:44:29 +0200

    Test effective installed performance refresh help

--- tests/test-shopos-performance-readme.sh
+++ tests/test-shopos-performance-readme.sh
@@ -6,7 +6,7 @@ readme="$root/image/package/etc/msfixit-shopos/README-performance.txt"
 
 test -s "$readme"
 grep -Fq 'Profiles: efficiency, balanced, performance' "$readme"
-grep -Fq 'sudo systemctl start msfixit-performance-profile.service' "$readme"
+grep -Fq 'sudo systemctl start msfixit-performance-refresh.service' "$readme"
 grep -Fq 'sudo msfixit-hardware-report' "$readme"
 grep -Fq 'does not overclock' "$readme"
 
